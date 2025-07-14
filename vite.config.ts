e40e231bb126f61e322aaf042a6426e9fa598cf8
Anbor 管理者 <haisou@anbor.co.jp>
2025-07-14 20:21:02 +0900

    Fix: set base './' to avoid blank screen on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
